cf2b1082a6b34a28f1c56406ccfca76f31cc10fa
maaaashi <doyaseizin.7@gmail.com>
2023-08-24 08:22:16 +0900

    置けるところのみ石が置けるように

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -2,6 +2,7 @@ import { Cell } from '@/Domains/Cell'
 import { Game } from '@/Domains/Game'
 import { Turn } from '@/Domains/Turn'
 import { gameAtom } from '@/atoms/GameAtom'
+import { canPlace } from '@/libs/canPlace'
 import { FC } from 'react'
 import { useRecoilState } from 'recoil'
 import { v4 as uuidV4 } from 'uuid'
@@ -15,6 +16,7 @@ export const Board: FC<Props> = ({ board }) => {
 
   const cellClick = (row: number, col: number) => {
     const board = gameState.lastTurn().board
+    if (!canPlace(board, row, col, gameState.lastTurn().nextDisc)) return
 
     const newBoard = [...Array(8)].map((_, r) => {
       return [...Array(8)].map((_, c) => {
@@ -27,6 +29,7 @@ export const Board: FC<Props> = ({ board }) => {
 
     const nextDisc =
       gameState.lastTurn().nextDisc === 'black' ? 'white' : 'black'
+
     const addTurn = new Turn(
       uuidV4(),
       gameState.turns.length,
@@ -36,6 +39,12 @@ export const Board: FC<Props> = ({ board }) => {
     const newGame = new Game([...gameState.turns, addTurn])
 
     setGameState(newGame)
+  }
+
+  const clickEnabled = (cell: Cell, row: number, col: number): boolean => {
+    if (cell.view()) return true
+
+    return !canPlace(board, row, col, gameState.lastTurn().nextDisc)
   }
 
   return (
@@ -49,11 +58,11 @@ export const Board: FC<Props> = ({ board }) => {
                   className='border w-10 h-10 flex justify-center items-center bg-white'
                   key={`col_${i}`}
                 >
-                  {cell.view() ? (
+                  {clickEnabled(cell, index, i) ? (
                     cell.view()
                   ) : (
                     <div
-                      className='w-8 h-8 hover:bg-base-200 cursor-pointer'
+                      className='w-8 h-8 bg-base-200 hover:bg-base-300 cursor-pointer'
                       onClick={() => {
                         cellClick(index, i)
                       }}
---
+++ src/libs/canPlace.ts
@@ -0,0 +1,48 @@
+import { Cell } from '@/Domains/Cell'
+
+const directions = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+]
+
+export const canPlace = (
+  board: Cell[][],
+  row: number,
+  col: number,
+  cell: 'black' | 'white'
+): boolean => {
+  const opponent = cell === 'black' ? 'white' : 'black'
+  let result = false
+
+  for (const [dx, dy] of directions) {
+    let nRow = row + dx
+    let nCol = col + dy
+
+    if (nRow < 0 || 8 <= nRow || nCol < 0 || 8 <= nCol) {
+      continue
+    }
+
+    if (board[nRow][nCol].cell !== opponent) {
+      continue
+    }
+
+    nRow += dx
+    nCol += dy
+
+    while (nRow >= 0 && nRow < 8 && nCol >= 0 && nCol < 8) {
+      if (board[nRow][nCol].cell === cell) {
+        result = true
+      }
+      nRow += dx
+      nCol += dy
+    }
+  }
+
+  return result
+}
