3ac8ff6b8da508f65758bcc05a21296fb81b30d0
maaaashi <doyaseizin.7@gmail.com>
2023-08-23 12:11:12 +0900

    型エラー解消

--- src/Driver/GameDriver.ts
+++ src/Driver/GameDriver.ts
@@ -2,7 +2,7 @@ import { Game } from '@/Domains/Game'
 import { GameGatewayInputPort } from '@/Gateway/Port/GameGatewayInputPort'
 
 export class GameDriver implements GameGatewayInputPort {
-  startGame(): Game {
+  registerGame(): Promise<Response> {
     throw new Error('未実装')
   }
 }
