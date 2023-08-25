cc845c2304e9dc02119ffa7d359b911e99d47899
maaaashi <doyaseizin.7@gmail.com>
2023-08-25 22:27:50 +0900

    set stone

--- src/app/api/send-chatgpt/route.ts
+++ src/app/api/send-chatgpt/route.ts
@@ -2,12 +2,14 @@ import { Configuration, OpenAIApi } from 'openai'
 import { NextResponse, NextRequest } from 'next/server'
 
 export const POST = async (req: NextRequest) => {
-  const { board } = await req.json()
+  const { board, stone } = await req.json()
   const apiKey = process.env.NEXT_PUBLIC_API_KEY
   const configuration = new Configuration({
     apiKey,
   })
   const openai = new OpenAIApi(configuration)
+
+  const nextStone = stone === 'black' ? -1 : 1
 
   const response = await openai.createChatCompletion({
     model: 'gpt-4',
@@ -18,7 +20,7 @@ export const POST = async (req: NextRequest) => {
         content: `What is the next move in Othello?
       The current board is as follows
       ${board}
-      I play as White (1). Where is the best place to place the stone?
+      I play as ${stone} (${nextStone}). Where is the best place to place the stone?
 
       [row][col] = `,
       },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 
+import { Game } from '@/Domains/Game'
+import { Turn } from '@/Domains/Turn'
 import { gameAtom } from '@/atoms/GameAtom'
 import { Board } from '@/components/Board'
-import { useEffect, useMemo, useState } from 'react'
-import { useRecoilValue } from 'recoil'
+import { flipCells } from '@/libs/flipCells'
+import { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { v4 as uuidV4 } from 'uuid'
 
 export default function Home() {
-  const game = useRecoilValue(gameAtom)
+  const [game, setGame] = useRecoilState(gameAtom)
 
   const [board, setBoard] = useState(game.lastTurn().board)
   const [nextDisc, setNextDisc] = useState(game.lastTurn().nextDiscView)
@@ -20,13 +24,27 @@ export default function Home() {
   const gpt = async () => {
     const body = {
       board: game.lastTurn().getBoardString(),
+      stone: game.lastTurn().nextDisc,
     }
     const res = await fetch('/api/send-chatgpt', {
       method: 'POST',
       body: JSON.stringify(body),
     })
     const json = await res.json()
-    console.log(json)
+
+    const row = json[1]
+    const col = json[4]
+
+    const board = game.lastTurn().board
+
+    const newBoard = flipCells([...board], row, col, game.lastTurn().nextDisc)
+
+    const nextDisc = game.lastTurn().nextDisc === 'black' ? 'white' : 'black'
+
+    const addTurn = new Turn(uuidV4(), game.turns.length, newBoard, nextDisc)
+    const newGame = new Game([...game.turns, addTurn])
+
+    setGame(newGame)
   }
 
   return (
