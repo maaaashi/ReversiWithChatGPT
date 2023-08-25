5631bef75bc8d45e66cc4c963a3be030d48a777a
maaaashi <doyaseizin.7@gmail.com>
2023-08-25 22:13:55 +0900

    fix build error

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
